a52a8726bdac00b0ed05f44bcdd382cc09908826
Joseph <joseph.e.oxford@gmail.com>
2026-02-05 16:34:10 -0800

    Added comment to room

--- basicserver/src/main/java/com/svgs/Room.java
+++ basicserver/src/main/java/com/svgs/Room.java
@@ -2,11 +2,11 @@ package com.svgs;
 
 public class Room {
     //public long roomID;
-    public String player1;
+    public String player1; //Username
     public String player2;
-    public int[][] ships1;
-    public int[][] ships2;
-    public int[][] guessBoard1;
+    public int[][] ships1; //This has 0, no ship, 1 there is a ship,
+    public int[][] ships2; //2, there is a destroyed ship, and 3, the opponent missed here
+    public int[][] guessBoard1; //0, blank, 1 miss, 2 hit
     public int[][] guessBoard2;
     public String turn;
 
